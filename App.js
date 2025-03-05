7be8f0c03d10c1caca3f4fd47d889ce286e7ff1c
Simran Arora <146839706+arorasimran624@users.noreply.github.com>
2025-03-05 12:34:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ResumeBuilder from "./ResumeBuilder"; // Adjust path as needed
+
+function App() {
+  return (
+    <div>
+      
+      <ResumeBuilder />
+
+
+    </div>
+  );
+}
+
+export default App;
